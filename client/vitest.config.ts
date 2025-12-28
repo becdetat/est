bc41cc659da5ed61d82263663a06bb8d437c5314
Rebecca Scott <rebecca.scott@sixpivot.com.au>
2025-12-28 13:31:18 +1000

    ok we're just hacking around now

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     plugins: [react()] as any,
     test: {
         globals: true,
